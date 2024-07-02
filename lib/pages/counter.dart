4317e618cf5d9550f73d609cafd51de8f7380af1
Omr Abdullah <121067316+omr-ql@users.noreply.github.com>
2024-07-02 09:52:56 +0300

    Update counter.dart
    add reset and decrement buttons to my app

--- lib/pages/counter.dart
+++ lib/pages/counter.dart
@@ -18,6 +18,23 @@ class _CounterState extends State<Counter> {
     });
   }
 
+  // function to reset the counter
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  // function to decrement the counter
+
+  void _decrementCounter () {
+    setState(() {
+      _counter -- ;
+    });
+    if (_counter < 0) {
+      _counter = 0 ;
+    }
+  }
   // UI (User Interface):
   @override
   Widget build(BuildContext context) {
@@ -38,23 +55,50 @@ class _CounterState extends State<Counter> {
             children: <Widget>[
               const Text(
                 'You have pushed it this many times:',
-                style: TextStyle(fontSize: 20),
+                style: TextStyle(fontSize: 18),
               ),
               const SizedBox(height: 5), // Add space between text and counter
               Text(
                 _counter.toString(),
                 style: TextStyle(fontSize: 40), // Make the counter bold
               ),
-              const SizedBox(height: 5), // Add space between counter and button
-              ElevatedButton(
-                onPressed: _incrementCounter,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue, // Set the button color to blue
-                ),
-                child: const Text(
-                  'Press!',
-                  style: TextStyle(color: Colors.white),
-                ),
+              const SizedBox(height: 10), // Add space between counter and buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: _resetCounter,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green, // Set the button color to red
+                    ),
+                    child: const Text(
+                      'Reset',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: _incrementCounter,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // Set the button color to blue
+                    ),
+                    child: const Text(
+                      'Press!',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: _decrementCounter,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red, // Set the button color to red
+                    ),
+                    child: const Text(
+                      'Decrement',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
